92f11acb0c62397a9cb3a295873a2802afa115d3
Jonathan Lee <jonathan.lee.devel@gmail.com>
2023-10-28 02:37:35 +0100

    Added permanent redirect for sign-out to Kinde

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -4,7 +4,6 @@ import { ArrowRight, Menu } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import { useEffect, useState } from 'react'
-import {LogoutLink} from "@kinde-oss/kinde-auth-nextjs/server";
 
 const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
     const [isOpen, setOpen] = useState<boolean>(false)
@@ -68,11 +67,11 @@ const MobileNav = ({ isAuth }: { isAuth: boolean }) => {
                                 </li>
                                 <li className='my-3 h-px w-full bg-gray-300' />
                                 <li>
-                                    <LogoutLink
+                                    <Link
                                         className='flex items-center w-full font-semibold'
-                                        href='/'>
+                                        href='/sign-out'>
                                         Sign out
-                                    </LogoutLink>
+                                    </Link>
                                 </li>
                             </>
                         )}
